dac63b48cca004a394c17fd629a3603fff357f38
Rafael Ega Krisaditya <144150187+krisadityabcde@users.noreply.github.com>
2024-04-24 13:49:44 +0700

    Update management.c

--- soal_2/management.c
+++ soal_2/management.c
@@ -12,141 +12,214 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include <ctype.h>
+#include <pwd.h>
 
-void donlot() {
+void buatLog(char *username, char *filename, char *action)
+{
     pid_t pid = fork();
 
-    if (pid == 0) {
+    if (pid == 0)
+    {
+        FILE *f = fopen("/home/rafaelega24/SISOP/modul2/2/history.log", "a");
+        time_t t = time(NULL);
+        struct tm tm = *localtime(&t);
+        fprintf(f, "[%s][%02d:%02d:%02d] - %s - %s\n", username, tm.tm_hour, tm.tm_min, tm.tm_sec, filename, action);
+        fclose(f);
+    }
+    else
+    {
+        int stat;
+        waitpid(pid, &stat, 0);
+    }
+}
+
+void donlot()
+{
+    pid_t pid = fork();
+
+    if (pid == 0)
+    {
         FILE *out = fopen("/home/rafaelega24/SISOP/modul2/2/log.txt", "w");
-        if (out == NULL) {
+        if (out == NULL)
+        {
             perror("Gabisa buka log");
             exit(EXIT_FAILURE);
         }
         dup2(fileno(out), 1);
         dup2(fileno(out), 2);
         fclose(out);
-    
         char *donlot[] = {"wget", "https://drive.google.com/uc?export=download&id=1rUIZmp10lXLtCIH3LAZJzRPeRks3Crup", "-O", "/home/rafaelega24/SISOP/modul2/2/library.zip", NULL};
         execvp("wget", donlot);
-    } else {
+    }
+    else
+    {
         int stat;
         waitpid(pid, &stat, 0);
     }
 }
 
-void unjip(){
+void unjip()
+{
     pid_t pid = fork();
-    
-    if(pid == 0) {
+
+    if (pid == 0)
+    {
         FILE *out = fopen("/home/rafaelega24/SISOP/modul2/2/log.txt", "w");
-        if (out == NULL) {
+        if (out == NULL)
+        {
             perror("Gabisa buka log");
             exit(EXIT_FAILURE);
         }
         dup2(fileno(out), 1);
         dup2(fileno(out), 2);
         fclose(out);
-        
-        char *unjip[] = {"unzip", "/home/rafaelega24/SISOP/modul2/2/library.zip", "-d", "/home/rafaelega24/SISOP/modul2/2/", NULL};
+
+        char *unjip[] = {"unzip", "/home/rafaelega24/SISOP/modul2/2/library.zip", "-d", "/home/rafaelega24/SISOP/modul2/2", NULL};
         execvp("unzip", unjip);
-    }else{
+    }
+    else
+    {
         int stat;
         waitpid(pid, &stat, 0);
     }
 }
 
-void dekrip(char *direktori){
+void dekrip(char *direktori)
+{
     struct dirent *entry;
     DIR *dir = opendir(direktori);
-    
+
     pid_t pid = fork();
-    
-    if(pid == 0){
-        while ((entry = readdir(dir)) != NULL){
-             if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0){
+
+    if (pid == 0)
+    {
+        while ((entry = readdir(dir)) != NULL)
+        {
+            if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
+            {
                 char ch, text[100];
                 int key = 19;
                 char oldFileName[500], newFileName[500];
                 sprintf(oldFileName, "%s/%s", direktori, entry->d_name);
                 strcpy(text, entry->d_name);
 
-                    if(text[0] < '0' || text[0] > '9'){
-                        for (int i = 0; text[i] != '\0'; ++i) {
+                if (text[0] < '0' || text[0] > '9')
+                {
+                    for (int i = 0; text[i] != '\0'; ++i)
+                    {
 
-                            ch = text[i];
-                            // Check for valid characters.
-                            if (isalnum(ch)) {
-                                //Lowercase characters.
-                                if (islower(ch)) {
-                                    ch = (ch - 'a' - key + 26) % 26 + 'a';
-                                }
-                                // Uppercase characters.
-                                if (isupper(ch)) {
-                                    ch = (ch - 'A' - key + 26) % 26 + 'A';
-                                }
+                        ch = text[i];
+                        // Check for valid characters.
+                        if (isalnum(ch))
+                        {
+                            // Lowercase characters.
+                            if (islower(ch))
+                            {
+                                ch = (ch - 'a' - key + 26) % 26 + 'a';
                             }
-                            // Adding decoded character back.
-                            text[i] = ch;
-
-                            sprintf(newFileName, "%s/%s", direktori, text);
+                            // Uppercase characters.
+                            if (isupper(ch))
+                            {
+                                ch = (ch - 'A' - key + 26) % 26 + 'A';
+                            }
                         }
-                        rename(oldFileName, newFileName);
+                        // Adding decoded character back.
+                        text[i] = ch;
+
+                        sprintf(newFileName, "%s/%s", direktori, text);
                     }
+                    rename(oldFileName, newFileName);
+                }
             }
         }
-        closedir(dir); 
-    }else{
+        closedir(dir);
+    }
+    else
+    {
         int stat;
         waitpid(pid, &stat, 0);
     }
 }
 
-void rinemDanDilet(char *direktori){
+void rinemDanDilet(char *direktori)
+{
     struct dirent *entry;
     DIR *dir = opendir(direktori);
-    
+
     pid_t pid = fork();
-    
-    if(pid == 0){
-        while ((entry = readdir(dir)) != NULL){
-            if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0){
+
+    if (pid == 0)
+    {
+        while ((entry = readdir(dir)) != NULL)
+        {
+            if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
+            {
                 char filenya[100];
                 char namaLama[500], namaBaru[500];
                 strcpy(filenya, entry->d_name);
 
-                if(strstr(filenya, "d3Let3") != NULL){
+                if (strstr(filenya, "d3Let3") != NULL)
+                {
                     sprintf(namaLama, "%s/%s", direktori, entry->d_name);
                     remove(namaLama);
-                }else if (strstr(filenya, "r3N4mE") != NULL){
+                    buatLog(getpwuid(getuid())->pw_name, entry->d_name, "Successfully deleted.");
+                }
+                else if (strstr(filenya, "r3N4mE") != NULL)
+                {
                     sprintf(namaLama, "%s/%s", direktori, entry->d_name);
-                    if(strstr(filenya, ".ts") != NULL){
+                    if (strstr(filenya, ".ts") != NULL)
+                    {
                         sprintf(namaBaru, "%s/helper.ts", direktori);
                         rename(namaLama, namaBaru);
-                    }else if(strstr(filenya, ".py") != NULL){
+                    }
+                    else if (strstr(filenya, ".py") != NULL)
+                    {
                         sprintf(namaBaru, "%s/calculator.py", direktori);
                         rename(namaLama, namaBaru);
-                    }else if(strstr(filenya, ".go") != NULL){
+                    }
+                    else if (strstr(filenya, ".go") != NULL)
+                    {
                         sprintf(namaBaru, "%s/server.go", direktori);
                         rename(namaLama, namaBaru);
-                    }else{
+                    }
+                    else
+                    {
                         sprintf(namaBaru, "%s/renamed.file", direktori);
                         rename(namaLama, namaBaru);
                     }
+                    buatLog(getpwuid(getuid())->pw_name, entry->d_name, "Successfully renamed.");
                 }else{
                     continue;
                 }
             }
         }
-        closedir(dir); 
-    }else{
+        closedir(dir);
+    }
+    else
+    {
         int stat;
         waitpid(pid, &stat, 0);
     }
 }
 
-void bekap(char *direktori){
+void buatFolder(char *direktori)
+{
+    pid_t pid = fork();
+
+    if (pid == 0)
+    {
+        char *buat[] = {"mkdir", "-p", direktori, NULL};
+        execvp("mkdir", buat);
+    }
+    else
+    {
+        int stat;
+        waitpid(pid, &stat, 0);
+    }
+}
+void pindahin(char *direktoriAsal, char *direktoriTujuan, char *action){
     struct dirent *entry;
-    DIR *dir = opendir(direktori);
+    DIR *dir = opendir(direktoriAsal);
 
     pid_t pid = fork();
 
@@ -154,14 +227,17 @@ void bekap(char *direktori){
             while ((entry = readdir(dir)) != NULL) {
                 if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
                     char filenya[500], dirFile[500];
-                    char dirBekap[500] = "/home/rafaelega24/SISOP/modul2/2/library/backup/";
-                    strcpy(filenya, entry->d_name);
+                    char dirTujuan[500];
+                    strcpy(dirTujuan, direktoriTujuan);
+                    strcat(dirTujuan, "/");
 
-                    
-                        strcpy(dirFile, direktori);
-                        strcat(dirFile, "/");
-                        strcat(dirFile, filenya);
-                       
+                    strcpy(filenya, entry->d_name);
+                    strcpy(dirFile, direktoriAsal);
+                    strcat(dirFile, "/");
+                    strcat(dirFile, filenya);
+
+                    if (strstr(filenya, "m0V3") != NULL){
+                        buatLog(getpwuid(getuid())->pw_name, filenya, action);
                         FILE *out = fopen("/home/rafaelega24/SISOP/modul2/2/log.txt", "w");
                         if (out == NULL) {
                             perror("Gabisa buka log");
@@ -171,14 +247,12 @@ void bekap(char *direktori){
                         dup2(fileno(out), 2);
                         fclose(out);
 
-                        char *pindah[] = {"resync", "-a", "--remove-source-files", dirFile, dirBekap, NULL};
-                        execvp("rsync", pindah);
-                        
-                        
-                    }else{
-                        continue;
-                    }
-                         
+                        char *pindah[] = {"mv", dirFile, dirTujuan, NULL};
+                        execvp("mv", pindah);
+                }else{
+                    continue;
+                }                     
+                }
             }
             closedir(dir);
         }else if(pid < 0){
@@ -188,29 +262,66 @@ void bekap(char *direktori){
         }
 }
 
+int mode, stop = 0;
+void jenisMode(int sig)
+{
+    pid_t pid = fork();
 
-int main(int argc, char *argv[]) {
+    if (pid == 0)
+    {
+        if (sig == SIGRTMIN)
+        {
+            mode = 0;
+        }
+        else if (sig == SIGUSR1)
+        {
+            mode = 1;
+        }
+        else if (sig == SIGUSR2)
+        {
+            mode = 2;
+        }
+        else if (sig == SIGTERM)
+        {
+            stop = 1;
+            exit(EXIT_SUCCESS);
+        }
+    }
+    else
+    {
+        int stat;
+        waitpid(pid, &stat, 0);
+    }
+}
+
+int main(int argc, char *argv[])
+{
     pid_t pid, sid;
     char libDir[] = "/home/rafaelega24/SISOP/modul2/2/library";
+    char bekapDir[] = "/home/rafaelega24/SISOP/modul2/2/library/backup";
 
     pid = fork();
 
-    if (pid < 0) {
+    if (pid < 0)
+    {
         exit(EXIT_FAILURE);
     }
 
-    if (pid > 0) {
+    if (pid > 0)
+    {
         exit(EXIT_SUCCESS);
     }
 
     umask(0);
 
     sid = setsid();
-    if (sid < 0) {
+    if (sid < 0)
+    {
         exit(EXIT_FAILURE);
     }
 
-    if ((chdir("/")) < 0) {
+    if ((chdir("/")) < 0)
+    {
         exit(EXIT_FAILURE);
     }
 
@@ -218,22 +329,37 @@ int main(int argc, char *argv[]) {
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
 
-    donlot();
-    unjip();
-    dekrip(libDir);
-    rinemDanDilet(libDir);
-    
-        if (argc > 1 && strcmp(argv[1], "-m") == 0) {
-            if (argc > 2 && strcmp(argv[2], "backup") == 0) {
-                for(int i = 0; i < 3; i++){
-                    sleep(1);
-                    bekap(libDir);
-                }
-               
+    signal(SIGRTMIN, jenisMode);
+    signal(SIGUSR1, jenisMode);
+    signal(SIGUSR2, jenisMode);
+    signal(SIGTERM, jenisMode);
 
-            } else if (strcmp(argv[2], "restore") == 0) {
-                //restore
-                }
+    if (argc == 3 && strcmp(argv[1], "-m") == 0)
+    {
+        if (strcmp(argv[2], "backup") == 0)
+        {
+            mode = 1;
         }
-    exit(EXIT_SUCCESS);
+        else if (strcmp(argv[2], "restore") == 0)
+        {
+            mode = 2;
+        }
+    }
+            donlot();
+            unjip();
+            dekrip(libDir);
+            rinemDanDilet(libDir);
+            buatFolder(bekapDir);
+
+    while (!stop){
+        if (mode == 1)
+        {
+            pindahin(libDir, bekapDir, "Successfully moved to backup.");
+        }
+        else if (mode == 2)
+        {
+            pindahin(bekapDir, libDir, "Successfully restored from backup.");
+        }
+        sleep(1);
+    }   
 }
